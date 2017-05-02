1ad4990791dadba5146fa04b37d0c88a2347283a
jayarellano6 <jaarellano@csumb.edu>
2017-05-02 09:47:10 -0700

    Update board.h

--- board.h
+++ board.h
@@ -2,10 +2,15 @@
 #define BOARD_H_INCLUDED
 #include "piece.h"
 
-class Board
-{
 private:
     Piece b[8][8];
+    bool isValidMove;
+    bool whosTurn;
+public:
+    Board();
+    void movePiece();
+    void setPieces();
+    int checkwin();
 
 };
 
