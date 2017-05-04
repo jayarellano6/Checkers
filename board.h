28d40d0e99c6bb6571760355b981e09391f6672c
jayarellano6 <jaarellano@csumb.edu>
2017-05-04 09:41:47 -0700

    Update board.h

--- board.h
+++ board.h
@@ -1,7 +1,10 @@
 #ifndef BOARD_H_INCLUDED
 #define BOARD_H_INCLUDED
 #include "piece.h"
+#include <iostream>
 
+class Board
+{
 private:
     Piece b[8][8];
     bool isValidMove;
@@ -10,8 +13,10 @@ public:
     Board();
     void movePiece();
     void setPieces();
+    void printBoard();
     int checkwin();
 
 };
 
 #endif // BOARD_H_INCLUDED
+
