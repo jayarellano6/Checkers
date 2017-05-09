f95c901537ed908d04a976c6d6e0cab2580c99e8
jayarellano6 <jaarellano@csumb.edu>
2017-05-09 15:10:16 -0700

    Update board.h

--- board.h
+++ board.h
@@ -6,7 +6,6 @@
 class Board
 {
 private:
-    Piece b[8][8];
     bool isValidMove;
     bool whosTurn;
 public:
@@ -16,11 +15,15 @@ public:
     void rightUpMove(int x, int y);
     void leftDownMove(int x, int y);
     void rightDownMove(int x, int y);
+    bool checkPieceKing(int x, int y);
+    bool checkPieceRed(int x, int y);
     void setPieces();
     void printBoard();
     void checkwin();
+    Piece b[8][8];
 
 };
 
 #endif // BOARD_H_INCLUDED
+
 
