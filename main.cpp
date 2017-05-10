5f869b29c5e97c12b09e42a2737c09f42db5278d
jayarellano6 <jaarellano@csumb.edu>
2017-05-09 17:09:55 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "board.h"
+#include "piece.h"
+#include "game.h"
+
+using namespace std;
+
+int main()
+{
+    Game g;
+    cout << "\t   Welcome to Checkers!\n\n";
+    g.playGame();
+
+}
