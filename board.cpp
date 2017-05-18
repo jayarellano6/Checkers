6dfd5b94bb3da29e5043e06c83bb0e2d65aebe9d
jayarellano6 <jaarellano@csumb.edu>
2017-05-18 09:04:42 -0700

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -3,17 +3,10 @@ using namespace std;
 
 Board::Board()
 {
-    setPieces();
-}
-void Board::swapValues(Piece& p1, Piece& p2)
-{
-    Piece x;
-    x = p1;
-    p1 = p2;
-    p2 = x;
-}
-void Board::setPieces()
-{
+    blk.setIsBlack(true);
+    blk.setIsTurn(false);
+    red.setIsBlack(false);
+    red.setIsTurn(true);
     b[0][1].setAllValues(0, 1, true, false, false);
     b[0][3].setAllValues(0, 3, true, false, false);
     b[0][5].setAllValues(0, 5, true, false, false);
@@ -44,10 +37,31 @@ void Board::setPieces()
     b[7][4].setAllValues(7, 4, true, true, false);
     b[7][6].setAllValues(7, 6, true, true, false);
 }
+void Board::swapValues(Piece& p1, Piece& p2)
+{
+    Piece x;
+    x = p1;
+    p1 = p2;
+    p2 = x;
+}
+
 void Board::printBoard()
 {
     int numbers[8] = {0,1,2,3,4,5,6,7};
-    cout <<"     | A   B   C   D   E   F   G   H |\n\n";
+    cout << "------------------------------------------\n";
+    cout << "|Scores" <<  endl;
+    cout << "|B player: [" << blk.getScore() << "] Pieces: ";
+    for(int i = 0; i < blk.getScore(); i++)
+    {
+        cout << " (r) ";
+    }
+    cout << "\n|R player: [" << red.getScore() << "] Pieces: ";
+    for(int i = 0; i < red.getScore(); i++)
+    {
+        cout << " (b) ";
+    }
+    cout << "\n------------------------------------------\n";
+    cout <<"\n     | A   B   C   D   E   F   G   H |\n\n";
     cout << char(196) <<"    ---------------------------------\n";
     for(int i = 0; i < 8; i++)
     {
@@ -68,20 +82,284 @@ void Board::printBoard()
         cout << endl;
     }
 }
-void Board::leftUpMove(int x, int y)
+
+bool Board::checkMove(int x, int y, int x1, int y1)
 {
-    swapValues(b[x-1][y-1], b[x][y]);
+    if(b[x1][y1].getIsPiece() == true)
+        return false;
+    //black piece
+    if(b[x][y].getIsRed() == false && b[x][y].getIsKing() == false){
+    if((x+1 == x1 && y + 1 == y1) || (x+1 == x1 && y - 1 == y1))
+    {
+        if(b[x1][y1].getIsPiece() == false)
+            return true;
+    }
+    else if ((x+2 == x1 && y + 2 == y1) || (x+2 == x1 && y - 2 == y1))
+    {
+        if((b[x + 1][y + 1].getIsPiece() || b[x + 1][y - 1].getIsPiece()) && (b[x + 1][y + 1].getIsRed() || b[x + 1][y - 1].getIsRed()))
+        {
+            //takes piece
+            if(b[x + 1][y + 1].getIsPiece())
+            {
+                Piece temp;
+                swapValues(temp, b[x + 1][y + 1]);
+                forceMove(x1, y1);
+            }
+            if(b[x + 1][y - 1].getIsPiece())
+            {
+                Piece temp;
+                swapValues(temp, b[x + 1][y - 1]);
+                forceMove(x1, y1);
+            }
+
+            blk.incrementScore();
+            return true;
+
+
+        }
+
+            // We could call an eaten function here?
+    }
+    }
+    //black king
+    if(b[x][y].getIsRed() == false && b[x][y].getIsKing() == true)
+    {
+        if((x-1 == x1 && y + 1 == y1) || (x-1 == x1 && y - 1 == y1))
+        {
+            if(b[x1][y1].getIsPiece() == false)
+                return true;
+        }
+        else if ((x-2 == x1 && y + 2 == y1) || (x-2 == x1 && y - 2 == y1))
+        {
+            if((b[x - 1][y + 1].getIsPiece() || b[x - 1][y - 1].getIsPiece()) && (b[x - 1][y + 1].getIsRed() || b[x - 1][y - 1].getIsRed()))
+            {
+                //take piece
+                if(b[x - 1][y + 1].getIsPiece())
+                {
+                    Piece temp;
+                    swapValues(temp, b[x - 1][y + 1]);
+                    forceMove(x1, y1);
+                }
+                if(b[x - 1][y - 1].getIsPiece())
+                {
+                    Piece temp;
+                    swapValues(temp, b[x - 1][y - 1]);
+                    forceMove(x1, y1);
+                }
+            blk.incrementScore();
+            return true;
+
+
+            }
+        }
+    }
+    //red king
+    if(b[x][y].getIsRed() == true && b[x][y].getIsKing() == true)
+    {
+        if((x+1 == x1 && y + 1 == y1) || (x+1 == x1 && y - 1 == y1))
+        {
+            if(b[x1][y1].getIsPiece() == false)
+                return true;
+        }
+        else if ((x+2 == x1 && y + 2 == y1) || (x+2 == x1 && y - 2 == y1))
+        {
+            if((b[x + 1][y + 1].getIsPiece() || b[x + 1][y - 1].getIsPiece()) && (!b[x + 1][y + 1].getIsRed() || !b[x + 1][y - 1].getIsRed()))
+            {
+            //takes piece
+                if(b[x + 1][y + 1].getIsPiece())
+                {
+                    Piece temp;
+                    swapValues(temp, b[x + 1][y + 1]);
+                    forceMove(x1, y1);
+                }
+                if(b[x + 1][y - 1].getIsPiece())
+                {
+                    Piece temp;
+                    swapValues(temp, b[x + 1][y - 1]);
+                    forceMove(x1, y1);
+                }
+            red.incrementScore();
+            return true;
+
+
+            }
+
+        }
+    }
+    //red piece
+    if(b[x][y].getIsRed() == true && b[x][y].getIsKing() == false){
+    if((x-1 == x1 && y + 1 == y1) || (x-1 == x1 && y - 1 == y1))
+    {
+        if(b[x1][y1].getIsPiece() == false)
+            return true;
+    }
+    else if ((x-2 == x1 && y + 2 == y1) || (x-2 == x1 && y - 2 == y1))
+    {
+        if((b[x - 1][y + 1].getIsPiece() || b[x - 1][y - 1].getIsPiece()) && (!b[x - 1][y + 1].getIsRed() || !b[x - 1][y - 1].getIsRed()))
+        {
+            //take piece
+            if(b[x - 1][y + 1].getIsPiece())
+            {
+                Piece temp;
+                swapValues(temp, b[x - 1][y + 1]);
+                forceMove(x1, y1);
+            }
+            if(b[x - 1][y - 1].getIsPiece())
+            {
+                Piece temp;
+                swapValues(temp, b[x - 1][y - 1]);
+                forceMove(x1, y1);
+            }
+            red.incrementScore();
+            return true;
+
+        }
+
+    }
+
+    }
+    if(b[x][y].getIsRed() == true && b[x][y].getIsKing() == false)
+    {
+        if((x1 > x || x1 == x || x1 <= (x-2)) || (y1 == y || (y1 >= (y+2)) || (y1 <= (y-2))))
+            return false;
+    }
+    if(b[x][y].getIsRed() == true && b[x][y].getIsKing() == true)
+    {
+        if((x1 == x || x1 <= (x-2)) || (y1 == y || (y1 >= (y+2)) || (y1 <= (y-2))))
+            return false;
+    }
+    if(b[x][y].getIsRed() == false && b[x][y].getIsKing() == false)
+    {
+        if((x1 < x || x1 == x) || (y1 == y))
+            return false;
+    }
+    if(b[x][y].getIsRed() == false && b[x][y].getIsKing() == true)
+    {
+        if((x1 == x) || (y1 == y))
+            return false;
+    }
+
+
 }
-void Board::rightUpMove(int x, int y)
+
+void Board::movePiece(int x, int y, int x1, int y1)
 {
-    swapValues(b[x-1][y+1], b[x][y]);
+    swapValues(b[x][y],b[x1][y1]);
+
 }
-void Board::rightDownMove(int x, int y)
+void Board::forceMove(int x, int y)
 {
-    swapValues(b[x+1][y+1], b[x][y]);
-}
-void Board::leftDownMove(int x, int y)
-{
-    swapValues(b[x+1][y-1], b[x][y]);
+		if(b[x][y].getIsKing() && b[x][y].getIsRed() && ((b[x + 1][y + 1]. getIsRed() == false && b[x+1][y+1].getIsPiece())||( b[x + 1][y- 1].getIsRed() == false && b[x+1][y-1].getIsPiece())||( b[x - 1][y + 1].getIsRed() == false && b[x-1][y+1].getIsPiece())|| (b[x- 1][y - 1].getIsRed() == false && b[x-1][y-1].getIsPiece())))
+		{
+
+			if((b[x-2][y + 2].getIsPiece() == false ))
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x-2][y + 2]);
+				swapValues(tempo,b[x - 1][y + 1] );
+				red.incrementScore();
+				forceMove(x-2 , y+2);
+			}
+			else if (b[x-2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x-2][y -2]);
+				swapValues(tempo,b[x - 1][y - 1] );
+				red.incrementScore();
+				forceMove(x-2 , y-2);
+			}
+			else if (b[x+2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x+2][y -2]);
+				swapValues(tempo,b[x + 1][y - 1] );
+				red.incrementScore();
+				forceMove(x+2 , y-2);
+			}
+			else if (b[x-2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x+2][y +2]);
+				swapValues(tempo,b[x + 1][y + 1] );
+				red.incrementScore();
+				forceMove(x-2 , y-2);
+			}
+		}
+
+		else if(b[x][y].getIsKing() && b[x][y].getIsRed() == false && ( (b[x + 1][y + 1].getIsRed() && b[x + 1][y + 1].getIsPiece()) ||  (b[x + 1][y - 1].getIsRed() && b[x + 1][y + 1].getIsPiece()) || (b[x - 1][y + 1].getIsRed() && b[x + 1][y + 1].getIsPiece()) || (b[x + 1][y + 1].getIsRed() && b[x - 1][y - 1].getIsPiece())))
+		{
+			if((b[x-2][y + 2].getIsPiece() == false ))
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x-2][y + 2]);
+				swapValues(tempo,b[x - 1][y + 1] );
+				blk.incrementScore();
+				forceMove(x-2 , y+2);
+			}
+			else if (b[x-2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x-2][y -2]);
+				swapValues(tempo,b[x - 1][y - 1] );
+				blk.incrementScore();
+				forceMove(x-2 , y-2);
+			}
+			else if (b[x+2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x+2][y -2]);
+				swapValues(tempo,b[x + 1][y - 1] );
+				blk.incrementScore();
+				forceMove(x+2 , y-2);
+			}
+			else if (b[x-2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x+2][y +2]);
+				swapValues(tempo,b[x + 1][y + 1] );
+				blk.incrementScore();
+				forceMove(x-2 , y-2);
+			}
+		}
+
+		else if(b[x][y].getIsKing() == false && b[x][y].getIsRed() == false && ( (b[x + 1][y + 1].getIsRed() && b[x + 1][y + 1].getIsPiece()) || (b[x + 1][y - 1].getIsRed() && b[x + 1][y - 1].getIsPiece())))
+		{
+			if (b[x+2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x+2][y -2]);
+				swapValues(tempo,b[x + 1][y - 1] );
+				blk.incrementScore();
+				forceMove(x+2 , y-2);
+			}
+			else if (b[x+2][y+2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x+2][y +2]);
+				swapValues(tempo,b[x + 1][y + 1] );
+				blk.incrementScore();
+				forceMove(x+2 , y+2);
+			}
+		}
+// Check piece
+		else if(b[x][y].getIsKing() == false && b[x][y].getIsRed() && ( (!b[x - 1][y + 1].getIsRed() && b[x - 1][y + 1].getIsPiece()) || (!b[x - 1][y - 1].getIsRed() && b[x - 1][y - 1].getIsPiece()) ))
+		{
+			if (b[x-2][y+2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x-2][y +2]);
+				swapValues(tempo,b[x - 1][y + 1] );
+				red.incrementScore();
+				forceMove(x-2 , y+2);
+			}
+			else if (b[x-2][y-2].getIsPiece() == false)
+			{
+				Piece tempo;
+				swapValues(b[x][y], b[x-2][y -2]);
+				swapValues(tempo,b[x - 1][y - 1] );
+				red.incrementScore();
+				forceMove(x-2 , y-2);
+			}
+		}
 }
 
